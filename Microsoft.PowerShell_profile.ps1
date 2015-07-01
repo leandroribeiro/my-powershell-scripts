cca6117a4df5e2606fb042a4d93bc43d03b948b4
Leandro Ribeiro <github@leandroribeiro.com>
2015-07-01 09:15:55 -0300

    Incluindo alias para obter todos os discos; Atribuindo diretório inicial como projetos;

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,19 +19,26 @@ Import-Module PsGet
 Import-Module PSReadLine
 Import-Module posh-git
 
-Set-PSReadlineOption -EditMode Emacs
-
-Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
+Set-Alias go-sublime "C:\Program Files\Sublime Text 3\sublime_text.exe"
+Set-Alias go-mu "C:\Program Files (x86)\muCommander\muCommander.exe"
+Set-Alias go-kee "c:\Tools\KeePass\KeePass.exe"
 
 function go-projetos
 {
-    set-location C:\Projetos\ 
+	set-location C:\Projetos\ 
 }
 
 function go-tools
 {
-    set-location C:\Tools\ 
+	set-location C:\Tools\ 
 }
+
+Function my-get-volumes
+{
+	GET-WMIOBJECT win32_logicaldisk | FORMAT-TABLE
+}
+
+New-Alias Get-Volumes my-get-volumes
 
 #----------------------------------------------------
 #TOUCH LIKE UNIX
@@ -39,78 +46,81 @@ function go-tools
 #New-Alias touch echo $null >>
 function Set-FileTime{
   param(
-    [string[]]$paths,
-    [bool]$only_modification = $false,
-    [bool]$only_access = $false
+	[string[]]$paths,
+	[bool]$only_modification = $false,
+	[bool]$only_access = $false
   );
 
   begin {
-    function updateFileSystemInfo([System.IO.FileSystemInfo]$fsInfo) {
-      $datetime = get-date
-      if ( $only_access )
-      {
-         $fsInfo.LastAccessTime = $datetime
-      }
-      elseif ( $only_modification )
-      {
-         $fsInfo.LastWriteTime = $datetime
-      }
-      else
-      {
-         $fsInfo.CreationTime = $datetime
-         $fsInfo.LastWriteTime = $datetime
-         $fsInfo.LastAccessTime = $datetime
-       }
-    }
+	function updateFileSystemInfo([System.IO.FileSystemInfo]$fsInfo) {
+	  $datetime = get-date
+	  if ( $only_access )
+	  {
+		 $fsInfo.LastAccessTime = $datetime
+	  }
+	  elseif ( $only_modification )
+	  {
+		 $fsInfo.LastWriteTime = $datetime
+	  }
+	  else
+	  {
+		 $fsInfo.CreationTime = $datetime
+		 $fsInfo.LastWriteTime = $datetime
+		 $fsInfo.LastAccessTime = $datetime
+	   }
+	}
    
-    function touchExistingFile($arg) {
-      if ($arg -is [System.IO.FileSystemInfo]) {
-        updateFileSystemInfo($arg)
-      }
-      else {
-        $resolvedPaths = resolve-path $arg
-        foreach ($rpath in $resolvedPaths) {
-          if (test-path -type Container $rpath) {
-            $fsInfo = new-object System.IO.DirectoryInfo($rpath)
-          }
-          else {
-            $fsInfo = new-object System.IO.FileInfo($rpath)
-          }
-          updateFileSystemInfo($fsInfo)
-        }
-      }
-    }
+	function touchExistingFile($arg) {
+	  if ($arg -is [System.IO.FileSystemInfo]) {
+		updateFileSystemInfo($arg)
+	  }
+	  else {
+		$resolvedPaths = resolve-path $arg
+		foreach ($rpath in $resolvedPaths) {
+		  if (test-path -type Container $rpath) {
+			$fsInfo = new-object System.IO.DirectoryInfo($rpath)
+		  }
+		  else {
+			$fsInfo = new-object System.IO.FileInfo($rpath)
+		  }
+		  updateFileSystemInfo($fsInfo)
+		}
+	  }
+	}
    
-    function touchNewFile([string]$path) {
-      #$null > $path
-      Set-Content -Path $path -value $null;
-    }
+	function touchNewFile([string]$path) {
+	  #$null > $path
+	  Set-Content -Path $path -value $null;
+	}
   }
  
   process {
-    if ($_) {
-      if (test-path $_) {
-        touchExistingFile($_)
-      }
-      else {
-        touchNewFile($_)
-      }
-    }
+	if ($_) {
+	  if (test-path $_) {
+		touchExistingFile($_)
+	  }
+	  else {
+		touchNewFile($_)
+	  }
+	}
   }
  
   end {
-    if ($paths) {
-      foreach ($path in $paths) {
-        if (test-path $path) {
-          touchExistingFile($path)
-        }
-        else {
-          touchNewFile($path)
-        }
-      }
-    }
+	if ($paths) {
+	  foreach ($path in $paths) {
+		if (test-path $path) {
+		  touchExistingFile($path)
+		}
+		else {
+		  touchNewFile($path)
+		}
+	  }
+	}
   }
 }
 
 New-Alias touch Set-FileTime
 #----------------------------------------------------
+
+# (Optional) Call default folder
+go-projetos
