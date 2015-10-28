f0b159393f4be562880108f8a4fbda7aae0fc39b
unknown <leandro.ribeiro@CAPPDESK035.cappta.com>
2015-10-28 12:05:51 -0200

    Including references

--- Check_NET_Framework_Installed.ps1
+++ Check_NET_Framework_Installed.ps1
@@ -1,4 +1,6 @@
-﻿Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
+﻿#References: http://stackoverflow.com/questions/3487265/powershell-script-to-return-versions-of-net-framework-on-a-machine
+
+Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse |
 Get-ItemProperty -name Version,Release -EA 0 |
 Where { $_.PSChildName -match '^(?!S)\p{L}'} |
 Select PSChildName, Version, Release
